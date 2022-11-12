14d3f5d624ce355035787a63f2a6c0dc36569170
chellengerKim <skkim27214@gmail.com>
2022-11-12 17:32:00 +0900

    일일퀘스트

---
+++ nest/hi-nest/src/test/app.e2e-sepc.ts
@@ -0,0 +1,66 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    await app.init();
+  });
+  it('/ (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Welcome to my Movie API');
+  });
+  
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())POST 201', (turn request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+  describe('/movies/:id', () => {GET 200', () => {turn request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it('PATCH 200', () => {turn request(app..patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });DELETE 200', () => turn request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
+  });
+});
+
